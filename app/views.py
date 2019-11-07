b5257a9060609d9f56b1013f93dcbab665c3a316
Student <student@donation.local>
2019-11-07 15:40:21 -0600

    completed work?

--- app/views.py
+++ app/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+from app.models import Album
+
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, "album_list.html", {"albums": albums})
+
+
+def album_detail(request, id):
+    album = Album.objects.get(id=id)
+    return render(request, "album_detail.html", {"album": album})
+
+
+def new_song(request):
+    pass
--- django_related_models_practice/urls.py
+++ django_related_models_practice/urls.py
@@ -14,6 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from app import views
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.album_list, name="album_list"),
+    path("album_detail/<id>", views.album_detail, name="album_detail"),
+]
